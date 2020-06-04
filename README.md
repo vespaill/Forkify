f10dd0cfd01e8e3d729d05e86f819c937f37afaf
Victor Espaillat <vicander_99@hotmail.com>
2020-06-04 09:42:53 -0400

    15.Building the Search controller

--- README.md
+++ README.md
@@ -8,3 +8,8 @@
 ### 14. Building the Search model
 
 -   How to use a simple data model using ES6 classes.
+
+### 15. Building the Search controller
+
+-   The concept of application state.
+-   A simple way of implementing state.
--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,34 @@
 import Search from './models/Search';
+import $ from 'jquery';
 
-const search = new Search('pizza');
-console.log(search);
+/**
+ * Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-search.getResults().then(() => console.log(search.result));
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = 'pizza';  // TODO
+
+    if (query) {
+        // 2) New search object and add to state
+        state.search = new Search(query);
+
+        // 3) Prepare the UI for results
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        console.log(state.search.result);
+    }
+}
+
+$('.search').on('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
