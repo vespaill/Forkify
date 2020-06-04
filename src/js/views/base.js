6131f01728dfede70140fcd93f24efd467e48912
Victor Espaillat <vicander_99@hotmail.com>
2020-06-04 10:43:48 -0400

    16.Building the search view—part 1

---
+++ src/js/views/base.js
@@ -0,0 +1,7 @@
+import $ from 'jquery'
+
+export const elements = {
+    searchForm: $('.search'),
+    searchInput: $('.search__field'),
+    searchResultList: $('.results__list')
+};
---
+++ src/js/views/searchView.js
@@ -0,0 +1,32 @@
+import { elements } from './base';
+
+export const getInput = () => elements.searchInput.val();
+
+export const clearInput = () => {
+    elements.searchInput.val('');
+};
+
+export const clearResults = () => {
+    elements.searchResultList.empty();
+};
+
+const renderRecipe = recipe => {
+    const markup = `
+        <li>
+            <a class="results__link" href="#${recipe.id}">
+                <figure class="results__fig">
+                    <img src="${recipe.image}" alt="Test">
+                </figure>
+                <div class="results__data">
+                    <h4 class="results__name">${recipe.title}</h4>
+                    <p class="results__author">${recipe.creditsText}</p>
+                </div>
+            </a>
+        </li>
+    `;
+    elements.searchResultList.append(markup);
+};
+
+export const renderResults = recipes => {
+    recipes.forEach(renderRecipe);
+};
